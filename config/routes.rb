4046fc8595d95056fd82409c2352173a1e40b3a2
lshapz <laurashapiro01@gmail.com>
2016-10-24 14:22:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions
   get :secret, to: "secrets#show"
   post '/logout' => 'sessions#destroy'
-  get '/login' => 'sessions#new', as: :login
+  get '/sessions/new' => 'sessions#new', as: :login
   post '/login' => 'sessions#create'
   resources :hello, to: "application#hello"
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe SessionsController do
   describe 'create' do
     it 'redirects to login page if :name is nil' do
       post :create
-      expect(response).to redirect_to :login
-      #controller: 'sessions', action: 'new'
+      expect(response).to redirect_to controller: 'sessions', action: 'new'
     end
 
     it 'redirects to login page if :name is empty' do
       post :create, name: ''
-      expect(response).to redirect_to :login
-      #controller: 'sessions', action: 'new'
+      expect(response).to redirect_to controller: 'sessions', action: 'new'
     end
 
     it 'sets session[:name] if :name was given' do
